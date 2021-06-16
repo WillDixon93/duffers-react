0e8297416a9d92c469a319e731ae3a379351c24d
Will Dixon <dixon.will1@gmail.com>
2021-06-16 09:37:00 +0100

    POST and GET working

--- duffer-functions/functions/index.js
+++ duffer-functions/functions/index.js
@@ -12,32 +12,39 @@ admin.initializeApp();
  });
 
  exports.getFores = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('fores').get()
-        .then(data => {
-            let fores = [];
-            data.forEach(doc => {
-                fores.push(doc.data());
-            });
-            return res.json(fores);
+    admin
+    .firestore()
+    .collection('fores')
+    .get()
+    .then(data => {
+        let fores = [];
+        data.docs.forEach(doc => {
+            fores.push(doc.data());
+        });
+        return res.json(fores);
         })
-        .catch(err => console.error(err));
+    .catch(err => console.error(err));
  });
 
- exports.createFores = functions.https.onRequest((req, res) => {
+ exports.createFore = functions.https.onRequest((req, res) => {
+     if(req.method !== 'POST'){
+         return res.status(400).json({ error: 'Method not allowed' });
+     }
    const newFore = {
-       body: req.body.body, 
-       userHandl: req.body.userHandle, 
+       body:req.body.body, 
+       userHandle: req.body.userHandle, 
        createdAt: admin.firestore.Timestamp.fromDate(new Date())
    };
 
-   admin.firestore()
-        .collection('fores')
-        .add(newFore)
-        .then(doc => {
-            res.json({ message: `document ${doc.id} created successfully`})
+   admin
+    .firestore()
+    .collection('fores')
+    .add(newFore)
+    .then(doc => {
+        res.json({ message: `document ${doc.id} created successfully`})
     })
-        .catch(err => {
-            res.status(500).json({ error: 'something went wrong' })
-            console.error(err);
-    })
+    .catch(err => {
+        res.status(500).json({ error: 'something went wrong' });
+        console.error(err)
+    }); 
  });
