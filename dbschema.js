0896939584fc43bda45499b2c468c1bb3c4256c6
Will Dixon <dixon.will1@gmail.com>
2021-06-17 12:58:43 +0100

    post and get requests fully working

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    fores: [
+        {
+            userHandle: 'user',
+            body: 'this is the fore body',
+            createdAt: '2021-06-27T12:48:50.018Z',
+            likeCount: 5,
+            commentCount: 2 
+        } 
+    ]
+}
--- duffer-functions/functions/index.js
+++ duffer-functions/functions/index.js
@@ -4,36 +4,50 @@ const { _onRequestWithOptions } = require("firebase-functions/lib/providers/http
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   response.send("Hello world!");
- });
+const config = {
+    apiKey: "AIzaSyAvkbvsET1cBfa_6PFXhvh7Cror_s6XKtQ",
+    authDomain: "scratch-312aa.firebaseapp.com",
+    databaseURL: "https://scratch-312aa-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "scratch-312aa",
+    storageBucket: "scratch-312aa.appspot.com",
+    messagingSenderId: "361791867022",
+    appId: "1:361791867022:web:dacb0495735eaa775821df",
+    measurementId: "G-P6Q4HR10XN"
+  };
 
- exports.getFores = functions.https.onRequest((req, res) => {
+
+const express = require ('express');
+const app = express();
+
+const firebase = require('firebase');
+firebase.initializeApp(config)
+
+app.get('/fores', (req, res) => {
     admin
     .firestore()
     .collection('fores')
+    .orderBy('createdAt', 'desc')
     .get()
     .then(data => {
         let fores = [];
-        data.docs.forEach(doc => {
-            fores.push(doc.data());
+        data.forEach(doc => {
+            fores.push({
+                foreId: doc.id,
+                body: doc.data(),
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
+            });
         });
         return res.json(fores);
         })
     .catch(err => console.error(err));
- });
+})
 
- exports.createFore = functions.https.onRequest((req, res) => {
-     if(req.method !== 'POST'){
-         return res.status(400).json({ error: 'Method not allowed' });
-     }
+ app.post('/fore', (req, res) => {
    const newFore = {
        body:req.body.body, 
        userHandle: req.body.userHandle, 
-       createdAt: admin.firestore.Timestamp.fromDate(new Date())
+       createdAt: new Date().toISOString()
    };
 
    admin
@@ -48,3 +62,8 @@ admin.initializeApp();
         console.error(err)
     }); 
  });
+
+
+ // https://baseurl.com/screams is not best practice. https://baseurl.com/api/ is. 
+
+ exports.api = functions.region('europe-west1').https.onRequest(app);
