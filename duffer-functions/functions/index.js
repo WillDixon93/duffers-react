7e5e4493934cdc569895de9c5d9a966e46987ebd
Will Dixon <dixon.will1@gmail.com>
2021-06-17 21:46:06 +0100

    middleware authentication - nearly complete - something funky happening with userHandle

--- duffer-functions/functions/index.js
+++ duffer-functions/functions/index.js
@@ -17,6 +17,7 @@ const config = {
   };
 
 const firebase = require('firebase');
+const { user } = require("firebase-functions/lib/providers/auth");
 firebase.initializeApp(config)
 
 const db = admin.firestore();
@@ -41,10 +42,42 @@ app.get('/fores', (req, res) => {
     .catch(err => console.error(err));
 })
 
- app.post('/fore', (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return res.status(403).json({ error: 'Unauthorised'});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log(decodedToken);
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token ', err);
+            return res.status(403).json(err);
+        })
+}
+
+ app.post('/fore', FBAuth, (req, res) => {
+     if (req.body.body.trim() === '') {
+         return res.status(400).json({ body: 'Body must not be empty' });
+     }
+
    const newFore = {
        body:req.body.body, 
-       userHandle: req.body.userHandle, 
+       userHandle: req.user.userHandle, 
        createdAt: new Date().toISOString()
    };
 
@@ -98,12 +131,11 @@ app.get('/fores', (req, res) => {
 
     if(Object.keys(errors).length > 0) return res.status(400).json(errors)
 
-     //TODO validate data
      let token, userId;
      db.doc(`/users/${newUser.handle}`).get()
         .then(doc => {
             if(doc.exists){
-                return res.status(400).json({ handle: 'this handle is already taken'});
+                return res.status(400).json({ handle: 'This handle is already taken'});
             } else {
                 return firebase
         .auth()
